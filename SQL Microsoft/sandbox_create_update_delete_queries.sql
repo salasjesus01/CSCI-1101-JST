811db93086bbc2219b11727d08bb6ba6de0e2312
salasjesus01 <salas.a.jesus01@gmail.com>
2022-04-28 18:30:55 -0500

    updated

---
+++ SQL Microsoft/sandbox_create_update_delete_queries.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+	sandbox(int32_value, bool_value, string_value)
+VALUES
+(151412, 0, 'Heyo')
+
+UPDATE
+	sandbox
+SET
+	int16_value = -1254,
+	datetime_value = '2010-00-15 04:08:19'
+WHERE
+	id = 5
+
+
+DELETE FROM 
+	sandbox
+WHERE
+	id = 9
