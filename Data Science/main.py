abc8b2cfeb77797b5aebed43a70b3c0382464265
salasjesus01 <salas.a.jesus01@gmail.com>
2022-05-02 12:49:02 -0500

    Corrected Main

--- Data Science/main.py
+++ Data Science/main.py
@@ -13,6 +13,22 @@ plt.show()
 
 BAR_WIDTH = 0.4
 
+df_main = pd.read_csv("game-ratings-by-top-10-genres.csv")
+
+df_follow = df_main.groupby(["genre_name"]) ["follow_count"].sum().reset_index() 
+df_follow = df_follow.rename(columns={"follow_count" : "total_follows"})
+df_hype = df_main.groupby(["genre_name"]) ["hype_count"].sum().reset_index() 
+df_hype = df_hype.rename(columns={"hype_count" : "total_hype"})
+
+plt.bar(df_follow.index - BAR_WIDTH / 2, df_follow["total_follows"], color = "blue", label = "Number of Follows", width = BAR_WIDTH )
+plt.bar(df_hype.index + BAR_WIDTH / 2, df_hype["total_hype"], color = "red", label = "Number of Hype", width = BAR_WIDTH )
+
+plt.xticks(df_follow.index, df_follow["genre_name"])
+
+plt.legend(loc="upper left")
+
+plt.show()
+
 df_main = pd.read_csv("game-ratings-by-top-10-platforms.csv")
 
 df_follow = df_main.groupby(["platform_name"]) ["follow_count"].sum().reset_index() 
